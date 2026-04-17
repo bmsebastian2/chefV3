552445c538049a017796606bdb6aad4d960a7645
SebastianBenitez <bmsebastian2@gmail.com>
2026-04-17 11:45:27 -0300

    Change wizzard no requerier logging

--- middleware.ts
+++ middleware.ts
@@ -36,6 +36,7 @@ export async function middleware(request: NextRequest) {
       // Si no está logueado y quiere entrar a rutas privadas → redirigir
       if (!user && !request.nextUrl.pathname.startsWith('/login') && 
           !request.nextUrl.pathname.startsWith('/auth') &&
+          !request.nextUrl.pathname.startsWith('/wizard') &&
           request.nextUrl.pathname !== '/') {
         return NextResponse.redirect(new URL('/login', request.url))
       }
