bf1ffb8ccc31fd8c086dfdaab48c0c5e3c27b15f
SebastianBenitez <bmsebastian2@gmail.com>
2026-06-18 11:02:06 -0300

    experimental inlinecss Turbopack

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   turbopack: {},
+  experimental: {
+    inlineCss: true,
+  },
   images: {
     formats: ["image/avif", "image/webp"],
     remotePatterns: [
